e725a3c4000b762228909ff9426e52a280a71672
anhdq1999 <18130006@st.hcmuaf.edu.vn>
2021-09-14 22:13:31 +0700

    take-rss-2

--- src/app/Component/kinh-doanh/kinh-doanh.component.ts
+++ src/app/Component/kinh-doanh/kinh-doanh.component.ts
@@ -11,7 +11,8 @@ export class KinhDoanhComponent implements OnInit {
   public dataNews : NewsRss;
 
   constructor(private service : ServesHttpService  ) { }
-  public url = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fvietnamnet.vn%2Frss%2Fkinh-doanh.rss';
+  // public url = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fvietnamnet.vn%2Frss%2Fthoi-su-chinh-tri.rss&api_key=rwr7qgkfpar8hckdq10q3ctab06e8fal6id1vkuh&order_dir=desc&count=20';
+  public url = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fvietnamnet.vn%2Frss%2Fkinh-doanh.rss&api_key=rwr7qgkfpar8hckdq10q3ctab06e8fal6id1vkuh&order_dir=desc&count=20';
   ngOnInit(): void {
     this.service.getAll(this.url).subscribe((data)=>{
       this.dataNews = data;
